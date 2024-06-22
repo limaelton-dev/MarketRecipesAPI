c2311b9668f86129e7a31b9ef738fb54e280245e
Elton Lima <eltonlima.contato@gmail.com>
2024-06-22 13:06:17 -0300

    fixed package errors

---
+++ MarketRecipesAPI/Data/MarketRecispesContext.cs
@@ -0,0 +1,16 @@
+﻿using MarketRecipesAPI.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketRecipesAPI.Data
+{
+    public class MarketRecipesContext : DbContext
+    {
+        public MarketRecipesContext(DbContextOptions<MarketRecipesContext> options)
+        : base(options) { }
+
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+
+}
